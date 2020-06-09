52cd12dfc1c01f551e3183644e66016105d3ff9c
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-09 08:53:52 +0100

    de linted code

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,24 +1,19 @@
+'use strict';
 describe("FizzBuzz", function(){
     
-    it ("should return 'Fizz' when number divisibale by 3", function(){
-        expect(fizzBuzz(3)).toEqual("Fizz");
+  it ("should return 'Fizz' when number divisible by 3", function(){
+      expect(fizzBuzz(3)).toEqual("Fizz");
+  });
 
-    });
+  it ("should return 'Buzz' when number is divisible by 5", function(){
+    expect(fizzBuzz(5)).toEqual("Buzz");
+  });
 
-    it ("should return 'Buzz' when number is devisable by 5", function(){
-        expect(fizzBuzz(5)).toEqual("Buzz");
+  it ("should return 'FizzBuzz when number is divisible by 3 and 5", function(){
+    expect(fizzBuzz(15)).toEqual("fizzBuzz");
+  });
 
-
-    });
-
-    it ("should return 'FizzBuzz when number is devisable by 3 and 5", function(){
-        expect(fizzBuzz(15)).toEqual("fizzBuzz");
-
-    });
-
-    it ("should return the number when not divisable by 3 or 5", function(){
-        expect(fizzBuzz(7)).toEqual(7)
-
-    });
-
+  it ("should return the number when not divisible by 3 or 5", function(){
+    expect(fizzBuzz(7)).toEqual(7)
+  });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,11 +1,13 @@
+'use strict';
+
 function fizzBuzz(num) {
-    if(num % 15 === 0) { 
-        return "fizzBuzz";
-    } else if(num % 3 === 0) {
-        return "Fizz";
-    } else  if(num % 5 === 0){
-        return "Buzz";
-    } else {
-        return num;
-    }
+  if(num % 15 === 0) { 
+    return "fizzBuzz";
+  } else if(num % 3 === 0) {
+    return "Fizz";
+  } else  if(num % 5 === 0){
+    return "Buzz";
+  } else {
+    return num;
+  }
 };
