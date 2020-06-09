7a6db8bda3fdd34f5aab0c9d2c89856aede29f0c
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-09 09:00:49 +0100

    first test for middleLetter complete

--- spec/middleLetterSpec.js
+++ spec/middleLetterSpec.js
@@ -1,1 +1,7 @@
 'use strict';
+
+describe('middleLetter', function() {
+  it ('Returns A when passed the letter A', function() {
+    expect(middleLetter('A')).toEqual('A')
+  });
+});
--- src/middleLetter.js
+++ src/middleLetter.js
@@ -1,1 +1,5 @@
 'use strict';
+
+function middleLetter(letter) {
+  return 'A';
+};
